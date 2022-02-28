c913a34112cba774c70c956b4ae20557a34df91f
juan david Iron-mind <davidtovarmontoya@gmail.com>
2022-02-28 17:17:09 -0500

    home and detali

--- MarvelApp/components/Detail.js
+++ MarvelApp/components/Detail.js
@@ -1,42 +1,40 @@
 
 import * as React from 'react';
-//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-//import MaterialCommunityIcons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/Ionicons';
 import { Text, View,  Image ,StyleSheet, TouchableOpacity} from 'react-native';
-
-//const Tab = createBottomTabNavigator();
+import Information from './Information.js'
+import Comics from './Comics.js'
+const Tab = createBottomTabNavigator();
 
 
 export default function Detail() {
-  return (<View>
-    <Text>hola</Text>
 
-    </View>)
-  // return (
-  //   <Tab.Navigator
-  //     initialRouteName="Information"
-  //     tabBarOptions={{
-  //       activeTintColor: 'darkred'
-  //     }}
-  //   >
-  //     <Tab.Screen
-  //       name="Information"
-  //       component={Information}
-  //       options={{
-  //         tabBarIcon: ({ color, size }) => (
-  //           <MaterialCommunityIcons name="information-circle" color={color} size={size} />
-  //         )
-  //       }}
-  //     />
-  //     <Tab.Screen
-  //       name="Comics"
-  //       component={Comics}
-  //       options={{
-  //         tabBarIcon: ({ color, size }) => (
-  //           <MaterialCommunityIcons name="book" color={color} size={size} />
-  //         )
-  //       }}
-  //     />
-  //   </Tab.Navigator>
-  //);
+  return (
+    <Tab.Navigator
+      initialRouteName="Information"
+      screenOptions={{
+        activeTintColor: 'darkred'
+      }}
+    >
+      <Tab.Screen
+        name="Information"
+        component={Information}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="information-circle" color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Comics"
+        component={Comics}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="book" color={color} size={size} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
--- MarvelApp/components/home.js
+++ MarvelApp/components/home.js
@@ -1,11 +1,52 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View , ActivityIndicator,FlatList} from 'react-native';
 import CharacterCard from './character';
-export default function Home(props) {
+// export default function Home(props) {
+//   return (
+//     <View style={{ flex: 1 }}>
+//     <CharacterCard {...props} image={require('../assets/favicon.png')} name='Iron Man' />
+//       <CharacterCard {...props} image={require('../assets/favicon.png')} name='Iron Mano' />
+//     </View>
+//   );
+// }
+
+import apiParams from '../config.js';
+import axios from 'axios';
+
+export default function Home() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const { ts, apikey, hash, baseURL } = apiParams;
+
+  useEffect(() => {
+    axios.get(`${baseURL}/v1/public/characters`, {
+      params: {
+        ts,
+        apikey,
+        hash
+      }
+    })
+      .then(response => setData(response.data.data.results))
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={{ flex: 1 }}>
-    <CharacterCard {...props} image={require('../assets/favicon.png')} name='Iron Man' />
-      <CharacterCard {...props} image={require('../assets/favicon.png')} name='Iron Mano' />
+    <View>
+      {isLoading
+        ? <ActivityIndicator size="large" color="#00ff00" />
+        : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }) => id.toString()}
+            renderItem={({ item }) => (
+              <CharacterCard
+                image={`${item?.thumbnail?.path}.${item?.thumbnail.extension}`}
+                name={item.name} />
+            )}
+          />
+        )
+      }
     </View>
   );
 }
